9a4c0072c4cf6ae6056f5303b9b98f13c92290a9
Krittam <krittam@gmail.com>
2017-07-30 13:50:07 +0530

    setup django project

---
+++ bot/members.py
@@ -0,0 +1,9 @@
+from channels import Group
+
+
+def ws_connect(message):
+    Group('users').add(message.reply_channel)
+
+
+def ws_disconnect(message):
+    Group('users').discard(message.reply_channel)
