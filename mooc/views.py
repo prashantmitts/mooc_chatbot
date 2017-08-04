2cd30d56e94e199cdf4a6087fe63875f00397377
Krittam <krittam@gmail.com>
2017-08-05 00:02:12 +0530

    change in html structure; chatbox placement

--- mooc/views.py
+++ mooc/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from mooc.models import Topic
 # Create your views here.
 
 def topic(request):
-    return render(request, 'mooc/topic.html',context={'topic_id':1})
+    topic_id = 1
+    topic = Topic.objects.filter(id=topic_id).first()
+    if topic:
+        topic_content = topic.content
+    else:
+        topic_content =None
+    return render(request, 'mooc/topic.html',context={'topic_id':topic_id, 'content': topic_content})
