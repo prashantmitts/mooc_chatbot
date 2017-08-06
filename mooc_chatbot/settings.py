599d26cb685e47b0d0c0075fb7246599a1ba3f28
Krittam <krittam@gmail.com>
2017-08-07 00:43:45 +0530

    test commit video

--- mooc_chatbot/settings.py
+++ mooc_chatbot/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "uploads/"), )
-print(STATICFILES_DIRS)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
