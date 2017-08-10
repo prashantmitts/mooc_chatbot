fdbd7aab04e54f9a045ed36709f8c931d873d235
Krittam <krittam@gmail.com>
2017-08-10 22:41:19 +0530

    fix print statement in settings.py; add MEDIA_ROOT

--- mooc_chatbot/settings.py
+++ mooc_chatbot/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "uploads/"), )
-print(STATICFILES_DIRS)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
